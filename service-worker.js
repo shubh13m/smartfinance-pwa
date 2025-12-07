ea28d90166d607e32c40f2ed39ffa3ba110833b5
Shubham Pandey <shubhpan1996@gmail.com>
2025-12-08 03:21:00 +0530

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'smartfinance-cache-v1';
+const ASSETS = ['index.html','style.css','app.js','db.js','manifest.json'];
+
+self.addEventListener('install', evt=>{
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', evt=>{
+  evt.respondWith(caches.match(evt.request).then(res=>res||fetch(evt.request)));
+});
+
+self.addEventListener('activate', evt=>{
+  evt.waitUntil(self.clients.claim());
+});
