78512c8672314f00f8a9cb7fcbe21fc2b9fc66d1
Shubham Pandey <shubhpan1996@gmail.com>
2025-12-09 15:26:20 +0530

    Add icons to service worker asset cache

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'smartfinance-cache-v1';
-const ASSETS = ['index.html','style.css','app.js','db.js','manifest.json'];
+const ASSETS = ['index.html','style.css','app.js','db.js','manifest.json','icon-192.png','icon-512.png'];
 
 self.addEventListener('install', evt=>{
   evt.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', evt=>{
-  evt.respondWith(caches.match(evt.request).then(res=>res||fetch(evt.request)));
+  evt.respondWith(caches.match(evt.request).then(res=>res || fetch(evt.request)));
 });
 
 self.addEventListener('activate', evt=>{
