0ce8327f5f566bf107a4668431e1faf7cece540e
Shubham Pandey <shubhpan1996@gmail.com>
2025-12-20 21:55:51 +0530

    Increment cache version to v12

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // --- CRITICAL CHANGE: INCREMENT THE CACHE VERSION ---
-const CACHE_NAME = 'smartfinance-cache-v11';
+const CACHE_NAME = 'smartfinance-cache-v12';
 // --------------------------------------------------
 
 const ASSETS = [
