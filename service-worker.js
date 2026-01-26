876218bafdceac2a93b553e519b6e8c5d9a44158
Shubham Pandey <shubhpan1996@gmail.com>
2026-01-26 12:33:22 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // --- CRITICAL CHANGE: INCREMENT THE CACHE VERSION ---
-const CACHE_NAME = 'smartfinance-cache-v22'; // Incremented for Phase 3
+const CACHE_NAME = 'smartfinance-cache-v23'; // Incremented for Phase 3
 // --------------------------------------------------
 
 const ASSETS = [
